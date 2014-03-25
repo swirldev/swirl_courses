c590e582f8c44cb48551f7fdaf99e92d09970d0a
Nick Carchedi <nick.carchedi@gmail.com>
2014-03-25 11:55:20 -0400

    add subsetting vectors lesson to r programming

---
+++ R_Programming/Subsetting_Vectors/initLesson.R
@@ -0,0 +1,5 @@
+# Put initialization code in this file. The variables you create
+# here will show up in the user's workspace when he or she begins
+# the lesson.
+
+x <- sample(c(rnorm(20), rep(NA, 20)))
