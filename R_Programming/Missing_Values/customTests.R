b86d04f859a2109fabdd234446397cdc4cf46261
wilcrofter <bill@crofter.net>
2014-03-25 13:49:00 -0400

    Added custom test, ANY_of_exprs(...), to Missing Values lesson. Updated lesson types to Coursera. Updated versions to 2.2.0. Filled in one author field.

--- R_Programming/Missing_Values/customTests.R
+++ R_Programming/Missing_Values/customTests.R
@@ -1,1 +1,6 @@
-# Put custom tests in this file.
+# Returns TRUE if e$expr matches any of the expressions given
+# (as characters) in the argument.
+ANY_of_exprs <- function(...){
+  e <- get("e", parent.frame())
+  any(sapply(c(...), function(expr)omnitest(expr)))
+}
