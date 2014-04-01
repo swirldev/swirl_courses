08e295c34fe8d12bd41bbb20d0dcc17ca951b779
wilcrofter <bill@crofter.net>
2014-04-01 11:12:15 -0400

    Count outcomes in progress.

--- Regression_Models/Count_Outcomes/initLesson.R
+++ Regression_Models/Count_Outcomes/initLesson.R
@@ -1,4 +1,4 @@
 # Put initialization code in this file.
-lgData <- read.csv(file.path(find.package("swirl"),
+hits <- read.csv(file.path(find.package("swirl"),
                              "Courses/Regression_Models/Count_Outcomes/leekGroupData.csv"), as.is=TRUE)
-lgData[,"date"] <- as.Date(lgData,"date")
+hits[,"date"] <- as.Date(hits,"date")
