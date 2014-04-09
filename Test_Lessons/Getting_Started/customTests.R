f48e27379492e46e43a3b02af2d39bf771d112dc
Nick Carchedi <nick.carchedi@gmail.com>
2014-04-09 12:53:42 -0400

    moved misc content to swirl_misc repo

--- Test_Lessons/Getting_Started/customTests.R
+++
@@ -1,9 +0,0 @@
-# This illustrates how to set the AUTO_DETECT_NEWVAR flag for the
-# duration of a lesson.
-AUTO_DETECT_NEWVAR <- FALSE
-
-# This illustrates a trivial custom test.
-testF <- function(){
-  print("Hello from customTests.R")
-  return(TRUE)
-}
