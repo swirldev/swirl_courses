eb9f7f7937b38c04ac432bc4850a60d4dbf59e3c
Gina <gina@crofter.org>
2014-04-19 19:01:51 -0400

    fixed problems with residual variation lesson and added dependson.txt file to least square lesson

---
+++ Regression_Models/Residual_Variation/anscombe.R
@@ -0,0 +1,28 @@
+require(stats); require(graphics)
+#summary(anscombe)
+
+##-- now some "magic" to do the 4 regressions in a loop:
+ff <- y ~ x
+mods <- setNames(as.list(1:4), paste0("lm", 1:4))
+for(i in 1:4) {
+  ff[2:3] <- lapply(paste0(c("y","x"), i), as.name)
+  ## or   ff[[2]] <- as.name(paste0("y", i))
+  ##      ff[[3]] <- as.name(paste0("x", i))
+  mods[[i]] <- lmi <- lm(ff, data = anscombe)
+  print(anova(lmi))
+}
+
+## See how close they are (numerically!)
+sapply(mods, coef)
+lapply(mods, function(fm) coef(summary(fm)))
+
+## Now, do what you should have done in the first place: PLOTS
+op <- par(mfrow = c(2, 2), mar = 0.1+c(4,4,1,1), oma =  c(0, 0, 2, 0))
+for(i in 1:4) {
+  ff[2:3] <- lapply(paste0(c("y","x"), i), as.name)
+  plot(ff, data = anscombe, col = "red", pch = 21, bg = "orange", cex = 1.2,
+       xlim = c(3, 19), ylim = c(3, 13))
+  abline(mods[[i]], col = "blue")
+}
+mtext("Anscombe's 4 Regression data sets", outer = TRUE, cex = 1.5)
+par(op)
--- Regression_Models/Residual_Variation/initLesson.R
+++ Regression_Models/Residual_Variation/initLesson.R
@@ -1,4 +1,5 @@
 galton <- UsingR::galton
+n <- 928
 est <- function(slope, intercept)intercept + slope*galton$parent
 sqe <- function(slope, intercept)sum( (est(slope, intercept)-galton$child)^2)
-fname <- paste(getwd(),"Regression_Models","Residuals_Variation","demofile.R",sep="/")
+fname <- paste(getwd(),"Regression_Models","Residual_Variation","anscombe.R",sep="/")
