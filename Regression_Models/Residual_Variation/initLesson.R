b1f9944cd49276f4a0bad968773127d744170d9c
Gina <gina@crofter.org>
2014-04-19 14:07:08 -0400

    fixed up lesson on residual variation

--- Regression_Models/Residual_Variation/initLesson.R
+++ Regression_Models/Residual_Variation/initLesson.R
@@ -1,3 +1,4 @@
 galton <- UsingR::galton
 est <- function(slope, intercept)intercept + slope*galton$parent
 sqe <- function(slope, intercept)sum( (est(slope, intercept)-galton$child)^2)
+fname <- paste(getwd(),"Regression_Models","Residuals_Variation","demofile.R",sep="/")
