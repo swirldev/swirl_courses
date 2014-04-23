683599b2a645e3cc1d6b58dbae51921b6f8caa4a
wilcrofter <bill@crofter.net>
2014-04-23 08:37:53 -0400

    under & over fitting in progress.

--- Regression_Models/Overfitting_and_Underfitting/fitting.R
+++ Regression_Models/Overfitting_and_Underfitting/fitting.R
@@ -1,9 +1,34 @@
-simdata <- {
+simbias <- function(){
   temp <- rnorm(100)
+  # Point A:
   x1 <- (temp + rnorm(100))/sqrt(2)
   x2 <- (temp + rnorm(100))/sqrt(2)
+  # Point B:
   x3 <- rnorm(100)
-  y <- x1 + x2 + x3 + rnorm(100)
-  data.frame(y=y, x1=x1, x2=x2, x3=x3)
+  # Function to simulate regression of y on 2 variables.
+  f <- function(k){
+    # Point C:
+    y <- x1 + x2 + x3 + .3*rnorm(100)
+    # Point D:
+    c(lm(y ~ x1 + x2)$coef[2],
+       lm(y ~ x1 + x3)$coef[2])
+  }
+  # Point E:
+  sapply(1:150, f)
 }
-# coef(y ~ 0 + x1 + x2 + x3) etc.
+
+# Have user do this: temp <- simbias()
+
+# wrap this into a function for the user to call
+p1 <- hist(temp[1,], plot=FALSE)
+p2 <- hist(temp[2,], plot=FALSE)
+yrange <- c(0, max(p1$counts, p2$counts))
+plot( p1, col=rgb(0,0,1,1/4), xlim=range(temp), ylim=yrange)
+plot( p2, col=rgb(1,0,0,1/4), xlim=range(temp), ylim=yrange, add=TRUE)
+
+# R^2 simulation
+# temp <- swiss
+# for(n in 1:41){temp[,paste0("random",n)] <- rnorm(nrow(temp))}
+# f <- function(n){summary(lm(Fertility ~ ., temp[,1:n])$r.squared)}
+# r.temp <- sapply(6:46, f)
+# plot(r.temp)
