9860b842b1d79a8becc10184c377e0c8efae14b9
wilcrofter <bill@crofter.net>
2014-04-29 08:06:37 -0400

    Added library(manipulate) to line 12 of initLesson.R in Least Squares estimation. This lesson does one thing in RStudio, with manipulate, and another without, so loading manipulate is conditional on the use of RStudio.

--- Regression_Models/Least_Squares_Estimation/initLesson.R
+++ Regression_Models/Least_Squares_Estimation/initLesson.R
@@ -9,6 +9,7 @@ gch_nor <- (gch - mean(gch))/sd(gch)
 #check to see if using RStudio and if pckg manipulate is available
 maniflg <- find.package("manipulate",quiet=TRUE)
 if  ((Sys.getenv("RSTUDIO") == "1")&&(nchar(maniflg)>0 )){
+  library(manipulate)
   fname <- paste(getwd(),"Regression_Models","Least_Squares_Estimation","slopedemo.R",sep="/")
 } else {
   fname <- paste(getwd(),"Regression_Models","Least_Squares_Estimation","slopedemo_no_mani.R",sep="/")  
