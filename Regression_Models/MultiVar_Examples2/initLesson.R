2c42f6155e7a444e06842a569bca71513c2ef011
Gina <gina@crofter.org>
2014-05-14 13:37:08 -0400

    fixes initLessons and dependson.txt

--- Regression_Models/MultiVar_Examples2/initLesson.R
+++ Regression_Models/MultiVar_Examples2/initLesson.R
@@ -1,28 +1,18 @@
 # Put initialization code in this file. The variables you create
 # here will show up in the user's workspace when he or she begins
 # the lesson.
-library(datasets); 
+
 data(InsectSprays); 
-require(stats); 
-require(graphics)
+sA <- InsectSprays$count[InsectSprays$spray=="A"]
+sB <- InsectSprays$count[InsectSprays$spray=="B"]
+sC <- InsectSprays$count[InsectSprays$spray=="C"]
+sD <- InsectSprays$count[InsectSprays$spray=="D"]
+sE <- InsectSprays$count[InsectSprays$spray=="E"]
+sF <- InsectSprays$count[InsectSprays$spray=="F"]
+
 
 boxplot(count ~ spray, data = InsectSprays,
         xlab = "Type of spray", ylab = "Insect count",
         main = "InsectSprays data", varwidth = TRUE, col = "lightgray")
 
-# Put initialization code in this file.
-# file.copy(from=file.path(find.package("swirl"),
-#                          "Courses/Regression_Models",
-#                          "MultiVar_Examples2/swissLMs.R"), 
-#           to="swissLMs.R")
-# file.edit("swissLMs.R")
-# source("swissLMs.R", globalenv())
 
-# if  ((Sys.getenv("RSTUDIO") == "1")&&(nchar(maniflg)>0 )){
-#   library(manipulate)
-#   fname <- paste(getwd(),"Regression_Models","Least_Squares_Estimation","slopedemo.R",sep="/")
-# } else {
-#   fname <- paste(getwd(),"Regression_Models","Least_Squares_Estimation","slopedemo_no_mani.R",sep="/")  
-# }
-# file.edit(fname)
-# fname2 <- paste(getwd(),"Regression_Models","Least_Squares_Estimation","finalplot.R",sep="/")
