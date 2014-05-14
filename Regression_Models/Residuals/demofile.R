45f68fd12feafc12ab6ba0bdfd59fab46fbbafcb
Gina <gina@crofter.org>
2014-05-14 15:02:48 -0400

    back to sourcing in figure questions

--- Regression_Models/Residuals/demofile.R
+++ Regression_Models/Residuals/demofile.R
@@ -1,1 +1,2 @@
 file.edit(fname)
+source(fname)
--- Regression_Models/Residuals/initLesson.R
+++ Regression_Models/Residuals/initLesson.R
@@ -3,12 +3,3 @@ est <- function(slope, intercept)intercept + slope*galton$parent
 sqe <- function(slope, intercept)sum( (est(slope, intercept)-galton$child)^2)
 datasets::attenu
 fname <- paste(find.package("swirl"),"Courses/Regression_Models","Residuals","res_eqn.R",sep="/")
-#Here are the vectors of variations or tweaks
-sltweak <- c(.01, .02, .03, -.01, -.02, -.03) #one for the slope
-ictweak <- c(.1, .2, .3, -.1, -.2, -.3)  #one for the intercept
-lhs <- numeric()
-rhs <- numeric()
-#left side of eqn is the sum of squares of residuals of the tweaked regression line
-for (n in 1:6) lhs[n] <- sqe(ols.slope+sltweak[n],ols.ic+ictweak[n])
-#right side of eqn is the sum of squares of original residuals + sum of squares of two tweaks
-for (n in 1:6) rhs[n] <- sqe(ols.slope,ols.ic) + sum(est(sltweak[n],ictweak[n])^2)
