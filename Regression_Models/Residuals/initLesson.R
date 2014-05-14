970d19e4377259d973e64bba11cf6dc0574207aa
Gina <gina@crofter.org>
2014-05-14 15:30:19 -0400

    fix

--- Regression_Models/Residuals/initLesson.R
+++ Regression_Models/Residuals/initLesson.R
@@ -3,3 +3,7 @@ est <- function(slope, intercept)intercept + slope*galton$parent
 sqe <- function(slope, intercept)sum( (est(slope, intercept)-galton$child)^2)
 datasets::attenu
 fname <- paste(find.package("swirl"),"Courses/Regression_Models","Residuals","res_eqn.R",sep="/")
+lhs <- numeric()
+rhs <- numeric()
+sltweak <- numeric()
+ictweak <- numeric()
