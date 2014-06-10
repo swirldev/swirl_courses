c918b8e8763d708d0d4acb56a759df7698459ade
wilcrofter <bill@crofter.net>
2014-06-10 12:15:40 -0400

    Custom omnitest which clears e$delta upon failure.

--- R_Programming/Vectors/customTests.R
+++ R_Programming/Vectors/customTests.R
@@ -28,3 +28,47 @@ expr_creates_var <- function(correctName=NULL){
   }
   return(results$passed)
 }
+
+omnitest <- function(correctExpr=NULL, correctVal=NULL, strict=FALSE){
+  e <- get("e", parent.frame())
+  # Trivial case
+  if(is.null(correctExpr) && is.null(correctVal))return(TRUE)
+  # Testing for correct expression only
+  if(!is.null(correctExpr) && is.null(correctVal)){
+    passed <- expr_identical_to(correctExpr)
+    if(!passed)e$delta <- list()
+    return(passed)
+  }
+  # Testing for both correct expression and correct value
+  # Value must be character or single number
+  valGood <- NULL
+  if(!is.null(correctVal)){
+    if(is.character(e$val)){
+      valResults <- expectThat(e$val,
+                               is_equivalent_to(correctVal, label=correctVal),
+                               label=(e$val))
+      if(is(e, "dev") && !valResults$passed)swirl_out(valResults$message)
+      valGood <- valResults$passed
+      # valGood <- val_matches(correctVal)
+    } else if(!is.na(e$val) && is.numeric(e$val) && length(e$val) == 1){
+      cval <- try(as.numeric(correctVal), silent=TRUE)
+      valResults <- expectThat(e$val, 
+                               equals(cval, label=correctVal),
+                               label=toString(e$val))
+      if(is(e, "dev") && !valResults$passed)swirl_out(valResults$message)
+      valGood <- valResults$passed
+    }
+  }
+  exprGood <- ifelse(is.null(correctExpr), TRUE, expr_identical_to(correctExpr))
+  if(valGood && exprGood){
+    return(TRUE)
+  } else if (valGood && !exprGood && !strict){
+    swirl_out("That's not the expression I expected but it works.")
+    swirl_out("I've executed the correct expression in case the result is needed in an upcoming question.")
+    eval(parse(text=correctExpr),globalenv())
+    return(TRUE)
+  } else {
+    e$delta <- list()
+    return(FALSE)
+  }
+}
