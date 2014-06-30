7e7f66f549ce503ec826a4d7f3c398c2e32128d8
Nick Carchedi <nick.carchedi@gmail.com>
2014-06-30 17:45:28 -0400

    change name of first apply fam lesson

--- R_Programming/lapply_and_sapply/initLesson.R
+++ R_Programming/lapply_and_sapply/initLesson.R
@@ -1,6 +1,6 @@
 # Path to installed lesson
 .lessonpath <- file.path(path.package("swirl"), "Courses", "R_Programming",
-                        "Loop_Functions_(Part_1)")
+                        "lapply_and_sapply")
 # Path to dataset
 .datapath <- file.path(.lessonpath, "flag.data.txt")
 # Load dataset
