84cadd883f42f364e5bd0e3377cd969b6109a912
Nick Carchedi <nick.carchedi@gmail.com>
2014-07-09 19:25:38 -0400

    more headway on simulation lesson

--- R_Programming/Simulation/customTests.R
+++ R_Programming/Simulation/customTests.R
@@ -1,7 +1,17 @@
-# Put custom tests in this file.
-
-# Uncommenting the following line of code will disable
-# auto-detection of new variables and thus prevent swirl from
-# repeating execution of printing and plotting commands.
-
-# AUTO_DETECT_NEWVAR <- FALSE
+match_call <- function(correct_call = NULL){
+  browser()
+  e <- get("e", parent.frame())
+  # Trivial case
+  if(is.null(correct_call)) return(TRUE)
+  # Fills out a function call with full argument names
+  match_call <- function(call) {
+    qcall <- substitute(call)
+    fun <- match.fun(qcall[[1]])
+    match.call(fun, qcall)
+  }
+  # Get full correct call
+  full_correct_call <- match_call(correct_call)
+  # Get full version of user's expression
+  full_user_expr <- match_call(e$expr)
+  identical(full_correct_call, full_user_expr)
+}
