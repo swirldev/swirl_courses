cbd89d1e17bb154f736709560fb92881165d46fa
Nick Carchedi <nick.carchedi@gmail.com>
2014-07-09 21:36:27 -0400

    fix bug in match_call custom test

--- R_Programming/Simulation/customTests.R
+++ R_Programming/Simulation/customTests.R
@@ -4,9 +4,15 @@ match_call <- function(correct_call = NULL) {
   if(is.null(correct_call)) return(TRUE)
   # Fills out a function call with full argument names
   expand_call <- function(call_string) {
+    # Quote expression
     qcall <- parse(text=call_string)[[1]]
+    # If expression is not greater than length 1...
     if(length(qcall) <= 1) return(qcall)
+    # Get function from function name
     fun <- match.fun(qcall[[1]])
+    # match.call() does not support primitive functions
+    if(is.primitive(fun)) return(qcall)
+    # Return expanded function call
     match.call(fun, qcall)
   }
   # Get full correct call
