cab231371435950a9aa13182edf48f20443030b5
Nick Carchedi <nick.carchedi@gmail.com>
2014-07-12 22:00:06 -0400

    done with dates and times

--- R_Programming/Dates_and_Times/initLesson.R
+++ R_Programming/Dates_and_Times/initLesson.R
@@ -1,3 +1,5 @@
+### MAY END UP USING THIS AT SOME POINT... ###
+
 # Code used to create dataset
 # dob_start <- as.Date('1950-01-01')
 # dob_end <- as.Date('1994-01-01')
@@ -12,7 +14,9 @@
 # eedata <- data.frame(ee_num = 1:99, dob, hire_date)
 # write.csv(eedata, file='R_Programming/Dates_and_Times/eedata.csv', 
 #           row.names=FALSE)
-
-.path2data <- file.path(path.package('swirl'), 'Courses', 'R_Programming',
-                       'Dates_and_Times', 'eedata.csv')
-eedata <- read.csv(.path2data, as.is=TRUE)
+# 
+# .path2data <- file.path(path.package('swirl'), 'Courses', 'R_Programming',
+#                        'Dates_and_Times', 'eedata.csv')
+# eedata <- read.csv(.path2data, as.is=TRUE)
+# eedata$dob <- as.Date(eedata$dob)
+# eedata$hire_date <- as.Date(eedata$hire_date)
