453c12fa16748e91a514506fa62e457402fb61c2
wilcrofter <bill@crofter.net>
2014-08-15 13:54:41 -0400

    fix typo

--- Regression_Models/Least_Squares_Estimation/initLesson.R
+++ Regression_Models/Least_Squares_Estimation/initLesson.R
@@ -1,7 +1,8 @@
 # Put initialization code in this file. The variables you create
 # here will show up in the user's workspace when he or she begins
 # the lesson.
-galton <- read.csv(file.path(find.package("swirl"), "Courses/Regression_Models/Introduction", "galton.csv"))gch <- galton$child
+galton <- read.csv(file.path(find.package("swirl"), "Courses/Regression_Models/Introduction", "galton.csv"))
+gch <- galton$child
 gpa <- galton$parent
 gpa_nor <- (gpa - mean(gpa))/sd(gpa)
 gch_nor <- (gch - mean(gch))/sd(gch)
