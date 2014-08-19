7bbc7481826389eaec81d99d6f4e22cb17447fc0
Nick Carchedi <nick.carchedi@gmail.com>
2014-08-19 09:15:01 -0400

    break ground on rprog alt version for email support

---
+++ R_Programming_Alt/Dates_and_Times/customTests.R
@@ -0,0 +1,53 @@
+# Put custom tests in this file.
+
+# Uncommenting the following line of code will disable
+# auto-detection of new variables and thus prevent swirl from
+# repeating execution of printing and plotting commands.
+
+# AUTO_DETECT_NEWVAR <- FALSE
+
+match_call <- function(correct_call = NULL) {
+  e <- get("e", parent.frame())
+  # Trivial case
+  if(is.null(correct_call)) return(TRUE)
+  # Get full correct call
+  full_correct_call <- expand_call(correct_call)  
+  # Expand user's expression
+  expr <- deparse(e$expr)
+  full_user_expr <- expand_call(expr)
+  # Compare function calls with full arg names
+  identical(full_correct_call, full_user_expr)
+}
+
+# Utility function for match_call answer test
+# Fills out a function call with full argument names
+expand_call <- function(call_string) {
+  # Quote expression
+  qcall <- parse(text=call_string)[[1]]
+  # If expression is not greater than length 1...
+  if(length(qcall) <= 1) return(qcall)
+  # See if it's an assignment
+  is_assign <- is(qcall, "<-")
+  # If assignment, process righthandside
+  if(is_assign) {
+    # Get righthand side
+    rhs <- qcall[[3]]
+    # Get function from function name
+    fun <- match.fun(rhs[[1]])
+    # match.call() does not support primitive functions
+    if(is.primitive(fun)) return(qcall)
+    # Get expanded call
+    full_rhs <- match.call(fun, rhs)
+    # Full call
+    qcall[[3]] <- full_rhs
+  } else { # If not assignment, process whole thing
+    # Get function from function name
+    fun <- match.fun(qcall[[1]])
+    # match.call() does not support primitive functions
+    if(is.primitive(fun)) return(qcall)
+    # Full call
+    qcall <- match.call(fun, qcall)
+  } 
+  # Return expanded function call
+  qcall
+}
---
+++ R_Programming_Alt/Matrices_and_Data_Frames/customTests.R
@@ -0,0 +1,86 @@
+expr_creates_var <- function(correctName=NULL){
+  e <- get("e", parent.frame())
+  # TODO: Eventually make auto-detection of new variables an option.
+  # Currently it can be set in customTests.R
+  delta <- if(!customTests$AUTO_DETECT_NEWVAR){
+    safeEval(e$expr, e)
+  } else {
+    e$delta
+  }
+  if(is.null(correctName)){
+    results <- expectThat(length(delta) >= 1,
+                          testthat::is_true(),
+                          label=paste(deparse(e$expr), 
+                                      "does not create a variable."))  
+  } else {
+    results <- expectThat(correctName %in% names(delta), 
+                          testthat::is_true(), 
+                          label=paste(deparse(e$expr),
+                                      "does not create a variable named",
+                                      correctName))
+  }
+  if(results$passed){
+    e$newVar <- e$val
+    e$newVarName <- names(delta)[1]
+    e$delta <- mergeLists(delta, e$delta)
+  } else {
+    e$delta <- list()
+  }
+  return(results$passed)
+}
+
+# Returns TRUE if the user has calculated a value equal to that calculated by the given expression.
+calculates_same_value <- function(expr){
+  e <- get("e", parent.frame())
+  # Calculate what the user should have done.
+  eSnap <- cleanEnv(e$snapshot)
+  val <- eval(parse(text=expr), eSnap)
+  passed <- isTRUE(all.equal(val, e$val))
+  if(!passed)e$delta <- list()
+  return(passed)
+}
+
+omnitest <- function(correctExpr=NULL, correctVal=NULL, strict=FALSE){
+  e <- get("e", parent.frame())
+  # Trivial case
+  if(is.null(correctExpr) && is.null(correctVal))return(TRUE)
+  # Testing for correct expression only
+  if(!is.null(correctExpr) && is.null(correctVal)){
+    passed <- expr_identical_to(correctExpr)
+    if(!passed)e$delta <- list()
+    return(passed)
+  }
+  # Testing for both correct expression and correct value
+  # Value must be character or single number
+  valGood <- NULL
+  if(!is.null(correctVal)){
+    if(is.character(e$val)){
+      valResults <- expectThat(e$val,
+                               is_equivalent_to(correctVal, label=correctVal),
+                               label=(e$val))
+      if(is(e, "dev") && !valResults$passed)swirl_out(valResults$message)
+      valGood <- valResults$passed
+      # valGood <- val_matches(correctVal)
+    } else if(!is.na(e$val) && is.numeric(e$val) && length(e$val) == 1){
+      cval <- try(as.numeric(correctVal), silent=TRUE)
+      valResults <- expectThat(e$val, 
+                               equals(cval, label=correctVal),
+                               label=toString(e$val))
+      if(is(e, "dev") && !valResults$passed)swirl_out(valResults$message)
+      valGood <- valResults$passed
+    }
+  }
+  exprGood <- ifelse(is.null(correctExpr), TRUE, expr_identical_to(correctExpr))
+  if(valGood && exprGood){
+    return(TRUE)
+  } else if (valGood && !exprGood && !strict){
+    swirl_out("That's not the expression I expected but it works.")
+    swirl_out("I've executed the correct expression in case the result is needed in an upcoming question.")
+    eval(parse(text=correctExpr),globalenv())
+    return(TRUE)
+  } else {
+    e$delta <- list()
+    return(FALSE)
+  }
+}
+
