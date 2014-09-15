dc9e78f75b36add32bb8a7b5153deae8ada311b5
seankross <smk240@gmail.com>
2014-09-15 19:56:08 -0400

    added functions lesson

--- R_Programming/Functions/customTests.R
+++ R_Programming/Functions/customTests.R
@@ -8,3 +8,14 @@ test_func1 <- function() {
   }, silent = TRUE)
   exists('ok') && isTRUE(ok)
 }
+
+test_func2 <- function() {
+  try({
+    func <- get('my_mean', globalenv())
+    t1 <- identical(func(9), mean(9))
+    t2 <- identical(func(1:10), mean(1:10))
+    t3 <- identical(func(c(-5, -2, 4, 10)), mean(c(-5, -2, 4, 10)))
+    ok <- all(t1, t2, t3)
+  }, silent = TRUE)
+  exists('ok') && isTRUE(ok)
+}
