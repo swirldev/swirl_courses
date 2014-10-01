be7fedb8b3705c9409eebae50a0a73bdd2e525ac
gina <gina@kuiper.lan>
2014-10-01 00:00:12 -0400

    got through normal distribution

--- Statistical_Inference/CommonDistros/plotNormal.R
+++ Statistical_Inference/CommonDistros/plotNormal.R
@@ -1,7 +1,8 @@
 x <- seq(-3, 3, length = 1000)
+dat <- data.frame(x=x, y=dnorm(x))
 library(ggplot2)
-g <- ggplot(data.frame(x = x, y = dnorm(x)), 
-              +             aes(x = x, y = y)) + geom_line(size = 2)
-g <- g + geom_vline(xintercept = -3 : 3, size = 2)
+g <- ggplot(dat, 
+              aes(x = x, y = y)) + geom_line(size = 2)
+g <- g + geom_vline(xintercept = -3 : 3, size = 1)
 g
 
--- Statistical_Inference/CommonDistros/testNormal.R
+++
@@ -1,4 +0,0 @@
-g <- ggplot(data.frame(x = x, y = dnorm(x)), 
-              +             aes(x = x, y = y)) + geom_line(size = 2)
-g <- g + geom_vline(xintercept = -3 : 3, size = 1)
-g
