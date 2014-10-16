123e72eec2b82fea61a8cc0832c359575d42a5b6
gina <gina@kuiper.lan>
2014-10-15 22:09:07 -0400

    finished first draft of hyp testing lesson, need to check it out

--- Statistical_Inference/Hypothesis_Testing/initLesson.R
+++ Statistical_Inference/Hypothesis_Testing/initLesson.R
@@ -2,16 +2,5 @@
 library(ggplot2)
 fname <- paste(getwd(),"Statistical_Inference/Hypothesis_Testing/father_son.csv",sep="/")
 fs <- read.csv(fname)
-mybin <- function{
-  rbind(
-    pbinom(-1, size = 8, p = .5, lower.tail = FALSE),
-    pbinom( 0, size = 8, p = .5, lower.tail = FALSE),
-    pbinom( 1, size = 8, p = .5, lower.tail = FALSE),
-    pbinom( 2, size = 8, p = .5, lower.tail = FALSE),
-    pbinom( 3, size = 8, p = .5, lower.tail = FALSE),
-    pbinom( 4, size = 8, p = .5, lower.tail = FALSE),
-    pbinom( 5, size = 8, p = .5, lower.tail = FALSE),
-    pbinom( 6, size = 8, p = .5, lower.tail = FALSE),
-    pbinom( 7, size = 8, p = .5, lower.tail = FALSE)
-)
-}
+quants <- c(-1:7)
+mybin <- pbinom(q,size=8,p=.5,lower.tail=FALSE)
