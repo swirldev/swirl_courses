8c1f64e38dca38f9cb5e38f2a0ca06e46ffb8d6b
gina <gina@kuiper.lan>
2014-11-06 20:50:14 -0500

    new custom test, obliterate, to remove large arrays to improve performance in resampling lesson

--- Statistical_Inference/Resampling/customTests.R
+++ Statistical_Inference/Resampling/customTests.R
@@ -1,6 +1,19 @@
 # So swirl does not repeat execution of plot commands
 AUTO_DETECT_NEWVAR <- FALSE
 
+obliterate <- function(vName){
+  e <- get("e", parent.frame())
+#   print(paste("Hello from obliterate",vName,sep=" "))
+#   if(exists(vName,envir=globalenv()))print(paste(vName," exists"))
+#   else print(paste(vName," doesn't exist"))
+  try(rm(list=c(vName),envir=globalenv()),silent=TRUE)
+#   if(exists(vName,envir=globalenv()))print(paste(vName," exists"))
+#   else print(paste(vName," doesn't exist"))
+  idx <- which(names(e$snapshot)==vName)
+#   print(paste("Hello from obliterate idx of ",vName," is ",idx,sep=" "))
+  try(e$snapshot <- e$snapshot[-idx],silent=TRUE)
+}
+
 # Returns TRUE if e$expr matches any of the expressions given
 # (as characters) in the argument.
 ANY_of_exprs <- function(...){
