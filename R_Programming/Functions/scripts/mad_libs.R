ad0591a9d7ed867e1de8df571cf930f273b4c02f
seankross <smk240@gmail.com>
2014-11-12 23:06:06 -0500

    still need to work on ellipses

---
+++ R_Programming/Functions/scripts/mad_libs.R
@@ -0,0 +1,14 @@
+# Let's explore how to "unpack" arguments from an ellipses when you use the
+# ellipses as an argument in a function. Below I have an example function that
+# is supposed to add two explicitly named arguments called alpha and beta.
+# 
+# add_alpha_and_beta <- function(...){
+#   # First we must capture the ellipsis inside of a list
+#   # and then assign the list to a variable. Let's name this
+#   # variable `args`.
+#   args <- list(...)
+#
+#   # 
+#
+#   alpha + beta 
+# }
