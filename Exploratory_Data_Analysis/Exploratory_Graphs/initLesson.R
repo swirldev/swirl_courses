11a7db983c8af7ae13a79591f8133a54ff32a544
gina <gina@kuiper.lan>
2014-11-24 23:08:42 -0500

    more work on the introduction.

--- Exploratory_Data_Analysis/Exploratory_Graphs/initLesson.R
+++ Exploratory_Data_Analysis/Exploratory_Graphs/initLesson.R
@@ -1,3 +1,5 @@
 library(ggplot2)
 # Put initialization code in this file.
 pollution <- read.csv("avgpm25.csv", colClasses = c("numeric", "character", "factor", "numeric", "numeric"))
+high <- pollution$pm25[pollution$pm25>15]
+low <- pollution$pm25[pollution$pm25<5]
