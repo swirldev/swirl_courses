9bb5c74efd10a871926afe26c7ee9f3ac7e74957
gina <gina@kuiper.lan>
2014-11-26 13:33:08 -0500

    fixed path to csv file

--- Exploratory_Data_Analysis/Exploratory_Graphs/initLesson.R
+++ Exploratory_Data_Analysis/Exploratory_Graphs/initLesson.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 # Put initialization code in this file.
-pollution <- read.csv("avgpm25.csv", colClasses = c("numeric", "character", "factor", "numeric", "numeric"))
+path_to_course <- file.path(find.package("swirl"),"Courses/Exploratory_Data_Analysis/Exploratory_Graphs/")
+pollution <- read.csv(paste(path_to_course,"avgpm25.csv"), colClasses = c("numeric", "character", "factor", "numeric", "numeric"))
 high <- pollution$pm25[pollution$pm25>15]
 low <- pollution$pm25[pollution$pm25<5]
 plot.new()
