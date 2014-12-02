e02da6949dc4b6d3ff06dfbbf793ab956cf4f9f3
wilcrofter <bill@crofter.net>
2014-12-02 16:31:22 -0500

    note on TFC

---
+++ Topics_in_Generalized_Linear_Models/Time_for_a_Change_Election_Model/customTests.R
@@ -0,0 +1,12 @@
+# Put custom tests in this file.
+
+# Uncommenting the following line of code will disable
+# auto-detection of new variables and thus prevent swirl from
+# executing every command twice, which can slow things down.
+
+# AUTO_DETECT_NEWVAR <- FALSE
+
+# However, this means that you should detect user-created
+# variables when appropriate. The answer test, creates_new_var()
+# can be used for for the purpose, but it also re-evaluates the
+# expression which the user entered, so care must be taken.
