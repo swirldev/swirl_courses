8597b0a76ca91965973d440fc2bf0c9d223fc5ba
gina <gina@kuiper.lan>
2014-12-04 12:56:04 -0500

    started plotting systems in r lesson, just setup, no substance

---
+++ Exploratory_Data_Analysis/Plotting_Systems/initLesson.R
@@ -0,0 +1,6 @@
+library(ggplot2)
+library(jpeg)
+library(lattice)
+# Put initialization code in this file.
+path_to_course <- file.path(find.package("swirl"),"Courses/Exploratory_Data_Analysis/Plotting_Systems")
+plot.new()
