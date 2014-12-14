85eabd1c71e113e867d99904fb43a2b73c3f7a5f
gina <gina@kuiper.lan>
2014-12-13 22:12:51 -0500

    finished graphics_devices, still testing basic plotting lesson

--- Exploratory_Data_Analysis/Graphics_Devices_in_R/initLesson.R
+++ Exploratory_Data_Analysis/Graphics_Devices_in_R/initLesson.R
@@ -4,6 +4,6 @@ data(cars)
 data(mpg)
 state <- data.frame(state.x77, region = state.region)
 # Put initialization code in this file.
-path_to_course <- file.path(find.package("swirl"),"Courses/Exploratory_Data_Analysis/Base_Plotting_System")
+path_to_course <- file.path(find.package("swirl"),"Courses/Exploratory_Data_Analysis/Graphics_Devices_in_R")
 dev.off()
 plot.new()
