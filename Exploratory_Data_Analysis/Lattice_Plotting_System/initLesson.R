2ce415dd4e718447fcab3d47fce257b56b9c8413
gina <gina@kuiper.lan>
2014-12-14 15:29:06 -0500

    finished testing base plot, started lattice plotting

---
+++ Exploratory_Data_Analysis/Lattice_Plotting_System/initLesson.R
@@ -0,0 +1,7 @@
+library(lattice)
+library(datasets)
+
+# Put initialization code in this file.
+path_to_course <- file.path(find.package("swirl"),"Courses/Exploratory_Data_Analysis/Lattice_Plotting_System")
+dev.off()
+plot.new()
