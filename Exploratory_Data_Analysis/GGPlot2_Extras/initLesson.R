835947127659e59e94eea903690a7b6af937ef3c
gina <gina@kuiper.lan>
2014-12-29 10:49:52 -0500

    split up part 1 lesson to create extras lesson (diamond example with qplot)

--- Exploratory_Data_Analysis/GGPlot2_Extras/initLesson.R
+++ Exploratory_Data_Analysis/GGPlot2_Extras/initLesson.R
@@ -18,12 +18,14 @@ fxfer <- function(fileName){
 
 
 
-
 myedit <- function(fname){
    #fxfer(fname)
    #file.edit(fname)
    mypath <- pathtofile(fname)
    file.edit(mypath)
 }
+brk <- numeric()
+for (n in 0:31) brk[n+1] <- (n*617)
+myhist <- hist(diamonds$price,breaks=brk,plot=FALSE)
+counts <- myhist$counts
 
-myhigh <- mpg$hwy
