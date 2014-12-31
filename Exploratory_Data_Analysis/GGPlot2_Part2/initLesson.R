8b388d1e8d3c580c430bcb661e0ae53300c18803
gina <gina@kuiper.lan>
2014-12-31 00:06:36 -0500

    ggplot2 part2 lesson

--- Exploratory_Data_Analysis/GGPlot2_Part2/initLesson.R
+++ Exploratory_Data_Analysis/GGPlot2_Part2/initLesson.R
@@ -2,13 +2,13 @@ library(ggplot2)
 library(datasets)
 
 # Put initialization code in this file.
-path_to_course <- file.path(find.package("swirl"),"Courses/Exploratory_Data_Analysis/GGPlot2_Part1")
+path_to_course <- file.path(find.package("swirl"),"Courses/Exploratory_Data_Analysis/GGPlot2_Part2")
 try(dev.off(),silent=TRUE)
 plot.new()
 
 pathtofile <- function(fileName){
   mypath <- file.path(find.package("swirl"),
-                      "Courses/Exploratory_Data_Analysis/GGPlot2_Extras/",
+                      "Courses/Exploratory_Data_Analysis/GGPlot2_Part1/",
                       fileName)
 }
 fxfer <- function(fileName){
@@ -18,14 +18,12 @@ fxfer <- function(fileName){
 
 
 
+
 myedit <- function(fname){
    #fxfer(fname)
    #file.edit(fname)
    mypath <- pathtofile(fname)
    file.edit(mypath)
 }
-brk <- numeric()
-for (n in 0:31) brk[n+1] <- (n*617)
-myhist <- hist(diamonds$price,breaks=brk,plot=FALSE)
-counts <- myhist$counts
 
+myhigh <- mpg$hwy
