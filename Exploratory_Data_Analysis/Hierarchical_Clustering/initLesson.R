21ba5ac2c59be14f44b21de66d7e0d2f6756a8a0
gina <gina@kuiper.lan>
2015-01-09 15:38:34 -0500

    tested hierarchical and kmeans lessons, started dimension reduction

--- Exploratory_Data_Analysis/Hierarchical_Clustering/initLesson.R
+++ Exploratory_Data_Analysis/Hierarchical_Clustering/initLesson.R
@@ -1,4 +1,3 @@
-library(ggplot2)
 library(fields)
 
 # Put initialization code in this file.
@@ -29,7 +28,7 @@ x <- rnorm(12,mean=rep(1:3,each=4),sd=0.2)
 y <- rnorm(12,mean=rep(c(1,2,1),each=4),sd=0.2)
 dataFrame <- data.frame(x=x,y=y)
 dataMatrix <- as.matrix(dataFrame)
-distxy <- dist(x,y)
+distxy <- dist(dataFrame)
 xsm <- c(x[4],x[8],x[11])
 ysm <- c(y[4],y[8],y[11])
 dFsm <- data.frame(x=xsm,y=ysm)
