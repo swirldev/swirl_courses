e7ba6881f42b43976b4809aabbec1122d62d511c
gina <gina@kuiper.lan>
2015-01-09 22:58:46 -0500

    more work on dim red lesson

---
+++ Exploratory_Data_Analysis/Dimension_Reduction/addPatt.R
@@ -0,0 +1,9 @@
+set.seed(678910)
+for(i in 1:40){
+  # flip a coin
+  coinFlip <- rbinom(1,size=1,prob=0.5)
+  # if coin is heads add a common pattern to that row
+  if(coinFlip){
+    dataMatrix[i,] <- dataMatrix[i,] + rep(c(0,3),each=5)
+  }
+}
