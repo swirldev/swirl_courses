81821cbd9f176cdf8ee4dd847d8a5acd5dd79823
wilcrofter <bill@crofter.net>
2015-02-12 15:38:19 -0500

    trap possible dev.off error with try clause

--- Exploratory_Data_Analysis/Graphics_Devices_in_R/initLesson.R
+++ Exploratory_Data_Analysis/Graphics_Devices_in_R/initLesson.R
@@ -5,5 +5,5 @@ data(mpg)
 state <- data.frame(state.x77, region = state.region)
 # Put initialization code in this file.
 path_to_course <- file.path(find.package("swirl"),"Courses/Exploratory_Data_Analysis/Graphics_Devices_in_R")
-dev.off()
+try(dev.off(), silent=TRUE)
 plot.new()
