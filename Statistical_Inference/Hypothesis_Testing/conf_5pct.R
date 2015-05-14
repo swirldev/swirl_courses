ef172fcadfa81bc73e5a39c07d874032a769fe28
pmcgee69 <paulmcgee1969@gmail.com>
2015-05-14 12:08:52 +0800

    Update conf_5pct.R

--- Statistical_Inference/Hypothesis_Testing/conf_5pct.R
+++ Statistical_Inference/Hypothesis_Testing/conf_5pct.R
@@ -1,7 +1,7 @@
 x <- seq(-8,8, length = 2000)
 dat <- data.frame(x=x, y=dnorm(x,sd=2))
 g <- ggplot(dat, aes(x = x, y = y)) + geom_line(size = 1.5)+scale_y_continuous(limits=c(0,max(dat$y)))
-suppressWarnings(g <- g+ layer("area",mapping = aes(x=ifelse(x>qnorm(.95,sd=2),x,NA)),
-            geom_params=list(fill="red",alpha=.5)) )
+g <- g + geom_ribbon(data = subset(dat,x>qnorm(0.975,sd=2)), mapping = aes(x = x, ymin=0, ymax = y, fill="red",alpha=.5))
+g <- g + theme(legend.position="none")
 suppressWarnings(print(g))
 
