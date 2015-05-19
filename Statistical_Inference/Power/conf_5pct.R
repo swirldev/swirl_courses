fafa95bf7d0c0cfeab58e8950a0c9fa153ad813f
pmcgee69 <paulmcgee1969@gmail.com>
2015-05-19 18:48:58 +0800

    A bit tidier

--- Statistical_Inference/Power/conf_5pct.R
+++ Statistical_Inference/Power/conf_5pct.R
@@ -1,9 +1,8 @@
 x <- seq(-4,4, length = 2000)
 dat <- data.frame(x=x, y=dnorm(x))
 g <- ggplot(dat, aes(x = x, y = y)) + geom_line(size = 1.5)+scale_y_continuous(limits=c(0,max(dat$y)))
-g <- g + geom_ribbon(data = subset(dat,x>qnorm(0.95)), mapping = aes(x = x, ymin=0, ymax = y, fill="red",alpha=.5))
+g <- g + geom_ribbon(data = subset(dat,x>qnorm(0.95)), aes(x = x, ymin=0, ymax = y), fill="red",alpha=.5)
 g <- g + theme(legend.position="none")
-#suppressWarnings(g <- g + geom_line(x=2.0,size=1.5,colour="blue"))
-suppressWarnings(print(g))
+print(g)
 
 
