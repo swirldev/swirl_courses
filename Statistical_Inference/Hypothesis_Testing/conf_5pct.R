6512ebd186fbcc5006feab0bb24643deefd32a99
Tussoch <tim@csat.co.nz>
2015-12-29 12:40:35 +0100

    Changed ggplot code to overcome error encountered by some Data Science Coursera users (at least 9 reported in current session) over several operating systems and versions of R. No other workaround found so far. Shading code adapted from http://stackoverflow.com/questions/12429333/how-to-shade-a-region-under-a-curve-using-ggplot2. I consider the suppressWarnings() is then no longer required.

--- Statistical_Inference/Hypothesis_Testing/conf_5pct.R
+++ Statistical_Inference/Hypothesis_Testing/conf_5pct.R
@@ -1,7 +1,13 @@
-x <- seq(-8,8, length = 2000)
-dat <- data.frame(x=x, y=dnorm(x,sd=2))
-g <- ggplot(dat, aes(x = x, y = y)) + geom_line(size = 1.5)+scale_y_continuous(limits=c(0,max(dat$y)))
-suppressWarnings(g <- g+ layer("area",mapping = aes(x=ifelse(x>qnorm(.95,sd=2),x,NA)),
-            geom_params=list(fill="red",alpha=.5)) )
-suppressWarnings(print(g))
-
+x <- seq(-8, 8, length = 2000)
+dat <- data.frame(x = x, y=dnorm(x, sd=2))
+g <- ggplot(dat, aes(x = x, y = y)) + geom_line(size = 1.5)+
+        scale_y_continuous(limits = c(0, max(dat$y)))
+p.level <- 0.95
+shade.start <- qnorm(p.level, sd = 2)
+y.up <-dnorm(shade.start, max(dat$x), 2)
+area.shade <- rbind(c(shade.start, 0), subset(dat, x >
+        shade.start), c(dat[nrow(dat), "x"], 0))
+g <- g + geom_segment(aes(x = shade.start,y = 0,xend = shade.start,
+        yend=y.up)) + geom_polygon(data = area.shade,
+        fill = "red", aes(x, y))
+print(g)
