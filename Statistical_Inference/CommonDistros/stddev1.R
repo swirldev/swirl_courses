010cab89ee357c6990091c79d52f4924d3ddf729
rsarkar0220 <resham.sarkar@gmail.com>
2016-01-05 12:34:40 -0600

    Update stddev1.R
    updated to replace geom_area by geom_ribbon

--- Statistical_Inference/CommonDistros/stddev1.R
+++ Statistical_Inference/CommonDistros/stddev1.R
@@ -1,7 +1,6 @@
-g <- g + geom_area(mapping = aes(x = ifelse(x > -1 & x < 1, x, 0)), fill = "red", alpha = 1)
-   scale_y_continuous(limits=c(0,max(dat$y)))
-   g <- g + geom_area(mapping = aes(x = ifelse(x > -2 & x < 2, x, 0)), fill = "red", alpha = 0.5)
-  g <- g + geom_area(mapping = aes(x = ifelse(x > -3 & x < 3, x, 0)), fill = "red", alpha = 0.35) 
-
+g <- ggplot(dat, aes(x = x, y = y)) + geom_line(size = 1.5)
+g <- g + geom_ribbon(aes(x = ifelse(x > -1 & x < 1, x, 0), ymin = 0, ymax = dat$y), fill = "red", alpha = 1)
+g <- g +  geom_ribbon(aes(x = ifelse(x > -2 & x < 2, x, 0), ymin = 0, ymax = dat$y), fill = "red", alpha = 0.5)
+g <- g +  geom_ribbon(aes(x = ifelse(x > -3 & x < 3, x, 0), ymin = 0, ymax = dat$y), fill = "red", alpha = 0.35)
 print(g)
 
