60235ee2befafebdeb52cf8e59191d531e0fe8ab
rsarkar0220 <resham.sarkar@gmail.com>
2016-01-05 12:31:37 -0600

    Update stddev1.R
    Updated to remove conflicts with the current versions of R.

--- Statistical_Inference/Asymptotics/stddev1.R
+++ Statistical_Inference/Asymptotics/stddev1.R
@@ -1,13 +1,7 @@
 x <- seq(-4, 4, length = 1000)
 dat <- data.frame(x=x, y=dnorm(x))
-g <- ggplot(dat, 
-            aes(x = x, y = y)) + geom_line(size = 1.5)
-g <- g+ layer("area",mapping = aes(x=ifelse(x>-1 & x<1,x,0)),
-            geom_params=list(fill="red",alpha=1)) +
-   scale_y_continuous(limits=c(0,max(dat$y)))
-g <- g+   layer("area",mapping = aes(x=ifelse(x>-2 & x<2 ,x,0)),
-                geom_params=list(fill="red",alpha=0.5))
-#g <- g+   layer("area",mapping = aes(x=ifelse(x>-3 & x<3 ,x,0)),
- #              geom_params=list(fill="red",alpha=0.35))
+g <- ggplot(dat, aes(x = x, y = y)) + geom_line(size = 1.5)
+g <- g + geom_ribbon(aes(x = ifelse(x > -1 & x < 1, x, 0), ymin = 0, ymax = dat$y), fill = "red", alpha = 1)
+g <- g +  geom_ribbon(aes(x = ifelse(x > -2 & x < 2, x, 0), ymin = 0, ymax = dat$y), fill = "red", alpha = 0.5)
 print(g)
 
