57f408537eb7a5033af863162f58f6ae84b22a33
Sean Kross <smk240@gmail.com>
2016-04-10 22:22:32 -0400

    Update customTests.R

--- R_Programming/Matrices_and_Data_Frames/customTests.R
+++ R_Programming/Matrices_and_Data_Frames/customTests.R
@@ -1,3 +1,14 @@
+ # Returns TRUE if the user has calculated a value equal to that calculated by the given expression.
+ calculates_same_value <- function(expr){
+   e <- get("e", parent.frame())
+   # Calculate what the user should have done.
+   eSnap <- cleanEnv(e$snapshot)
+   val <- eval(parse(text=expr), eSnap)
+   passed <- isTRUE(all.equal(val, e$val))
+   if(!passed)e$delta <- list()
+   return(passed)
+ }
+
 # Get the swirl state
 getState <- function(){
   # Whenever swirl is running, its callback is at the top of its call stack.
