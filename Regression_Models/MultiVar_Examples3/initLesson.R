69fb9a22833024a0f4ac806d6b271ad40f7d0fbf
Sean Kross <smk240@gmail.com>
2016-04-14 21:00:14 -0400

    Update initLesson.R

--- Regression_Models/MultiVar_Examples3/initLesson.R
+++ Regression_Models/MultiVar_Examples3/initLesson.R
@@ -9,9 +9,5 @@
 # here will show up in the user's workspace when he or she begins
 # the lesson.
 
-file.copy(from=file.path(file.path(.get_course_path(),
-	"Regression_Models","MultiVar_Examples3","hunger.csv"),
-          to="hunger.csv")
-                         
 hunger <-read.csv(file.path(.get_course_path(),
 	"Regression_Models","MultiVar_Examples3","hunger.csv"))
