1346276f3ec62400715f767d668f451d0e9cb54b
Valquiria Quirino <vfquirin@gmail.com>
2019-04-16 11:05:56 -0500

    Creating the first lesson

---
+++ R_Dev_Essentials/Create_a_package/initLesson.R
@@ -0,0 +1,4 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
