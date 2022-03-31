5c6da19d7bfd6187a11f1250a3e8118c076522a7
rg0 <rahul_burgeon@yahoo.in>
2022-04-01 00:54:03 +0530

    Update mad_libs-correct.R
    Error in args[["place"]] : object of type 'closure' is not subsettable
    Therefore only args[[i]] works

--- R_Programming_Alt/Functions/scripts/mad_libs-correct.R
+++ R_Programming_Alt/Functions/scripts/mad_libs-correct.R
@@ -30,9 +30,9 @@
 mad_libs <- function(...){
   # Do your argument unpacking here!
   args <- list(...)
-  place <- args[["place"]]
-  adjective <- args[["adjective"]]
-  noun <- args[["noun"]]
+  place <- args[[1]]
+  adjective <- args[[2]]
+  noun <- args[[3]]
   
   # Don't modify any code below this comment.
   # Notice the variables you'll need to create in order for the code below to
